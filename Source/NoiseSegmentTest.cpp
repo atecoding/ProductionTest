9f4575a5fed39b66829a51b1e146f02266fcc34d
Milo Street <milo@echoaudio.com>
2015-06-28 12:52:40 -0700

    New relative level test
    Added code to detect high noise regions and only use low level average
    in recordings where those occur

--- Source/NoiseSegmentTest.cpp
+++ Source/NoiseSegmentTest.cpp
@@ -21,7 +21,7 @@ NoiseSegmentTest::~NoiseSegmentTest()
 
 bool NoiseSegmentTest::calc(OwnedArray<AudioSampleBuffer> &buffs,String &msg, ErrorCodes &errorCodes)
 {
-    AudioSampleBuffer highPassOutput;
+    AudioSampleBuffer testInput, testOutput;
     IIRCoefficients highPassCoefficients(IIRCoefficients::makeHighPass(sample_rate, 500.0));
     IIRFilter highPassFilter;
 	bool pass = true;
@@ -35,16 +35,80 @@ bool NoiseSegmentTest::calc(OwnedArray<AudioSampleBuffer> &buffs,String &msg, Er
         int physicalInput = input + channel;
         AudioSampleBuffer *sourceBuffer = buffs[physicalInput];
 		int num_samples = getSamplesRequired();
+		int transitions = 0, lowCount = 0, highCount = 0;
+		int lowSamples = 0, highSamples = 0;
 		bool channelOK = false;
+		float totalAve = 0.0, runningAve = 0.0, lowAve = 0.0, highAve = 0.0, tops = 0.0, max = 0.0;
+
+		testInput.setSize(1, num_samples);
+		testInput.copyFrom(0, 0, *sourceBuffer, 0, 0, num_samples);
+		float *indata = testInput.getWritePointer(0);
+
+		highPassFilter.processSamples(indata, num_samples);
+
+		testOutput.setSize(1, num_samples);
+        testOutput.copyFrom(0, 0, *sourceBuffer, 0, 0, num_samples);
+        float *outdata = testOutput.getWritePointer(0);
+        
+		for (int i = 0; i < num_samples - 500; i++)
+		{
+			totalAve += fabs(indata[i]);
+			if (indata[i] > max)
+				max = indata[i];
+		}
+		totalAve /= num_samples;
+
+		for (int i = 0; i < num_samples - 500; i++)
+			{
+			tops *= 0.99;
+			if (indata[i] > tops)
+				tops = indata[i];
+			runningAve *= 0.96;
+//			runningAve += fabs(indata[i]) * 0.04;
+			runningAve += tops * 0.04;
+			//			if (0.1 * (runningAve / totalAve) < 0.1)
+//			if (tops < max/2)
+			if (runningAve < max/2)
+			{
+				highCount = 0;
+				if (0 == lowCount)
+					transitions++;
+				lowAve += fabs(indata[i]);
+				lowSamples++;
+				lowCount++;
+				outdata[i] = indata[i];
+			}
+			else
+			{
+				lowCount = 0;
+				if (0 == highCount)
+					transitions++;
+				highAve += fabs(indata[i]);
+				highSamples++;
+				highCount++;
+				outdata[i] = 0;
+			}
+		}
+		
+		lowAve /= (float)lowSamples;
+		highAve /= (float)highSamples;
+
+		float percentHigh = (float)highSamples / (float)(highSamples + lowSamples);
+		msg += String::formatted(T("Percent High = %2.0f"), percentHigh * 100.0f) + "%\n";
+
+		msg += String::formatted(T("transitions = %d"), transitions) + "\n";
+		float db = minusInfinity;
+		db = Decibels::gainToDecibels(totalAve, minusInfinity);
+		msg += "totalAve = " + Decibels::toString(db, 1, minusInfinity) + "\n";
+		db = minusInfinity;
+		db = Decibels::gainToDecibels(highAve, minusInfinity);
+		msg += "highAve = " + Decibels::toString(db, 1, minusInfinity) + "\n";
+		db = minusInfinity;
+		db = Decibels::gainToDecibels(lowAve, minusInfinity);
+		msg += "lowlAve = " + Decibels::toString(db, 1, minusInfinity) + "\n";
 
 #if 0
-		
-         
-		highPassOutput.setSize(1, num_samples);
-        highPassOutput.copyFrom(0, 0, *sourceBuffer, 0, 0, num_samples);
-        float *data = highPassOutput.getWritePointer(0);
-        
-        highPassFilter.processSamples(data, num_samples);
+		highPassFilter.processSamples(data, num_samples);
         
         float accum = 0.0f;
 		for (int i = num_samples / 16; i < 15 * num_samples / 16; i++)
@@ -61,65 +125,22 @@ bool NoiseSegmentTest::calc(OwnedArray<AudioSampleBuffer> &buffs,String &msg, Er
         
         float average = 0.0f;
         float db = minusInfinity;
-        if (highPassOutput.getNumSamples() != 0)
+        if (testOutput.getNumSamples() != 0)
         {
-            average = accum / highPassOutput.getNumSamples();
+            average = accum / testOutput.getNumSamples();
             db = Decibels::gainToDecibels(average, minusInfinity);
         }
-        
-        if (autoPass)
-        {
-            msg += Decibels::toString(db, 1, minusInfinity);
-            channelOK = true;
-            pass = true;
-        }
-        else
-        {
-            float delta = db - (float)(double)previousResultsTree[channelID];
-            
-            msg += Decibels::toString(db, 1, minusInfinity);
-            msg += " (" + Decibels::toString(delta, 1, minusInfinity) + ")";
-            
-            DBG("db:" << db << "  previous:" << (double)previousResultsTree[channelID] << " delta:" << delta);
-            
-            channelOK = min_level_db <= delta && delta <= max_level_db;
-            if (channelOK)
-            {
-                msg += " OK";
-            }
-            else
-            {
-                msg += " FAIL";
-                
-                errorCodes.add(ErrorCodes::LEVEL, physicalInput + 1);
-            }
-            pass &= channelOK;
-        }
-        
-        previousResultsTree.setProperty(channelID, db, nullptr);
-        
-        if (channel < (num_channels - 1))
-        {
-            msg += "\n";
-        }
-
-#endif
+#endif        
 
 #if WRITE_WAVE_FILES
-        if (false == channelOK)
+        if (1) // (false == channelOK)
         {
-            {
-                String name(title);
-
-                name += String::formatted(" (out%02d-in%02d).wav", output, physicalInput);
-                WriteWaveFile(unit, name, sample_rate, sourceBuffer, getSamplesRequired());
-            }
             
             {
                 String name(title);
                 
-                name += String::formatted(" highpass (out%02d-in%02d).wav", output, physicalInput);
-                WriteWaveFile(unit, name, sample_rate, &highPassOutput, getSamplesRequired());
+                name += String::formatted(" processed (out%02d-in%02d).wav", output, physicalInput);
+                WriteWaveFile(unit, name, sample_rate, &testOutput, getSamplesRequired());
             }
         }
 #endif
--- Source/RelativeLevelTest.cpp
+++ Source/RelativeLevelTest.cpp
@@ -33,8 +33,8 @@ RelativeLevelTest::~RelativeLevelTest()
 
 bool RelativeLevelTest::calc(OwnedArray<AudioSampleBuffer> &buffs,String &msg, ErrorCodes &errorCodes)
 {
-    AudioSampleBuffer highPassOutput;
-    IIRCoefficients highPassCoefficients(IIRCoefficients::makeHighPass(sample_rate, 500.0));
+	AudioSampleBuffer testInput, testOutput;
+	IIRCoefficients highPassCoefficients(IIRCoefficients::makeHighPass(sample_rate, 500.0));
     IIRFilter highPassFilter;
 	bool pass = true;
     ValueTree unitTree(unit->tree);
@@ -51,31 +51,73 @@ bool RelativeLevelTest::calc(OwnedArray<AudioSampleBuffer> &buffs,String &msg, E
         AudioSampleBuffer *sourceBuffer = buffs[physicalInput];
 		int num_samples = getSamplesRequired();
         bool channelOK = false;
-         
-		highPassOutput.setSize(1, num_samples);
-        highPassOutput.copyFrom(0, 0, *sourceBuffer, 0, 0, num_samples);
-        float *data = highPassOutput.getWritePointer(0);
-        
-        highPassFilter.processSamples(data, num_samples);
-        
-        float accum = 0.0f;
-		for (int i = num_samples / 16; i < 15 * num_samples / 16; i++)
-        {
-            float sample = fabs(*data);
-            
-            accum += sample;
-            
-            i++;
-            data++;
-        }
-        
-        msg += "    Channel " + String(physicalInput + 1) + ": ";
+		int transitions = 0, lowCount = 0, highCount = 0;
+		int lowSamples = 0, highSamples = 0;
+		float totalAve = 0.0, runningAve = 0.0, lowAve = 0.0, highAve = 0.0, tops = 0.0, max = 0.0;
+
+		testInput.setSize(1, num_samples);
+		testInput.copyFrom(0, 0, *sourceBuffer, 0, 0, num_samples);
+		float *indata = testInput.getWritePointer(0);
+
+		highPassFilter.processSamples(indata, num_samples);
+
+		testOutput.setSize(1, num_samples);
+		testOutput.copyFrom(0, 0, *sourceBuffer, 0, 0, num_samples);
+		float *outdata = testOutput.getWritePointer(0);
+
+		for (int i = 0; i < num_samples - 500; i++)
+		{
+			totalAve += fabs(indata[i]);
+			if (indata[i] > max)
+				max = indata[i];
+		}
+		totalAve /= num_samples;
+
+		for (int i = 0; i < num_samples - 500; i++)
+		{
+			tops *= 0.99;
+			if (indata[i] > tops)
+				tops = indata[i];
+			runningAve *= 0.96;
+			//			runningAve += fabs(indata[i]) * 0.04;
+			runningAve += tops * 0.04;
+			//			if (0.1 * (runningAve / totalAve) < 0.1)
+			//			if (tops < max/2)
+			if (runningAve < max / 2)
+			{
+				highCount = 0;
+				if (0 == lowCount)
+					transitions++;
+				lowAve += fabs(indata[i]);
+				lowSamples++;
+				lowCount++;
+				outdata[i] = 0;
+			}
+			else
+			{
+				lowCount = 0;
+				if (0 == highCount)
+					transitions++;
+//				highAve += fabs(indata[i]);
+//				highSamples++;
+				highCount++;
+				outdata[i] = max;
+			}
+		}
+
+		lowAve /= (float)lowSamples;
+//		highAve /= (float)highSamples;
+
+		msg += "    Channel " + String(physicalInput + 1) + ": ";
         
         float average = 0.0f;
         float db = minusInfinity;
-        if (highPassOutput.getNumSamples() != 0)
+        if (testInput.getNumSamples() != 0)
         {
-            average = accum / highPassOutput.getNumSamples();
+			if (transitions > 50)
+				average = totalAve;
+			else
+				average = lowAve;
             db = Decibels::gainToDecibels(average, minusInfinity);
         }
         
@@ -116,7 +158,7 @@ bool RelativeLevelTest::calc(OwnedArray<AudioSampleBuffer> &buffs,String &msg, E
         }
 
 #if WRITE_WAVE_FILES
-        if (false == channelOK)
+		if (false == channelOK)
         {
             {
                 String name(title);
@@ -128,8 +170,8 @@ bool RelativeLevelTest::calc(OwnedArray<AudioSampleBuffer> &buffs,String &msg, E
             {
                 String name(title);
                 
-                name += String::formatted(" highpass (out%02d-in%02d).wav", output, physicalInput);
-                WriteWaveFile(unit, name, sample_rate, &highPassOutput, getSamplesRequired());
+                name += String::formatted(" transitions (out%02d-in%02d).wav", output, physicalInput);
+				WriteWaveFile(unit, name, sample_rate, &testOutput, getSamplesRequired());
             }
         }
 #endif
